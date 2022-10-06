47952ee29fa5f46dacfca8bdea4a9e0060c298ba
Birrellc <vaiezmedia@gmail.com>
2022-10-06 16:40:10 +0100

    Feat: fetched data from api with serverSideProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,11 @@
-import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
 import Header from '../components/Header';
+import Banner from '../components/Banner';
 import styles from '../styles/Home.module.css';
+import requests from '../utils/requests';
 
-const Home: NextPage = () => {
+const Home = () => {
   return (
     <div className='relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010811]'>
       <Head>
@@ -16,6 +17,7 @@ const Home: NextPage = () => {
       <Header />
       <main>
         {/* Banner */}
+        <Banner />
         <section>
           {/* Movie Rows */}
           {/* Movie Rows */}
@@ -29,3 +31,42 @@ const Home: NextPage = () => {
 };
 
 export default Home;
+
+export const getServerSideProps = async () => {
+  const [
+    netflixOriginals,
+    trendingNow,
+    topRated,
+    actionMovies,
+    comedyMovies,
+    horrorMovies,
+    romanceMovies,
+    documentaries,
+  ] = await Promise.all([
+    // promise.all to avoid individual awaits due to many requests
+    fetch(requests.fetchNetflixOriginals).then((res) => res.json()),
+    fetch(requests.fetchTrending).then((res) => res.json()),
+    fetch(requests.fetchTopRated).then((res) => res.json()),
+    fetch(requests.fetchActionMovies).then((res) => res.json()),
+    fetch(requests.fetchComedyMovies).then((res) => res.json()),
+    fetch(requests.fetchHorrorMovies).then((res) => res.json()),
+    fetch(requests.fetchRomanceMovies).then((res) => res.json()),
+    fetch(requests.fetchDocumentaries).then((res) => res.json()),
+  ]);
+
+  // need to return probs for serverside rendering
+  // https://nextjs.org/docs/basic-features/data-fetching/get-server-side-props
+  return {
+    props: {
+      // fetch requests return id & results objects
+      netflixOriginals: netflixOriginals.results,
+      trendingNow: trendingNow.results,
+      topRated: topRated.results,
+      actionMovies: actionMovies.results,
+      comedyMovies: comedyMovies.results,
+      horrorMovies: horrorMovies.results,
+      romanceMovies: romanceMovies.results,
+      documentaries: documentaries.results,
+    },
+  };
+};
