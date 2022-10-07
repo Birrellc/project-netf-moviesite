e59af1a16e84a55acacc144a7eda017161f821f4
Birrellc <vaiezmedia@gmail.com>
2022-10-07 11:31:10 +0100

    Style: fixed banner image gradient clipping & changed bg color

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'gradient-to-b':
+          'linear-gradient(to bottom, rgba(20,20,20,.15) 15%,rgba(30,30,30,.30) 30%,rgba(30,30,30,.6) 55%,#191919 80%,#191919 100%);',
+      },
+    },
   },
   plugins: [
     require('tailwindcss-textshadow'),
