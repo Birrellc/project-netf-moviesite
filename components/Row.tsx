ff757cc5b150e1f5201a41f7482a33c86ece9dc7
Birrellc <vaiezmedia@gmail.com>
2022-10-07 10:20:23 +0100

    Feat: created carousel scrolling with chevrons for rows.tsx

--- components/Row.tsx
+++ components/Row.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import { useRef, useState } from 'react';
+import Thumbnail from './Thumbnail';
 import { Movie } from '../typings';
 import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/solid';
 
@@ -8,17 +9,50 @@ interface Props {
 }
 
 const Row = ({ title, movies }: Props) => {
+  // ref to target the div for chevron scrolling (carousel)
+  const rowRef = useRef<HTMLDivElement>(null);
+  // state for tracking if row scrolls
+  const [isMoved, setIsMoved] = useState(false);
+
+  // https://morayodeji.com/creating-a-carousel-component-using-react-hooks-and-sass
+  const clickHandler = (direction: string) => {
+    setIsMoved(true);
+    if (rowRef.current) {
+      const { scrollLeft, clientWidth } = rowRef.current;
+
+      const scrollTo =
+        direction === 'left'
+          ? scrollLeft - clientWidth
+          : scrollLeft + clientWidth;
+      //https://stackoverflow.com/questions/15935318/smooth-scroll-to-top
+      rowRef.current.scrollTo({ left: scrollTo, behavior: 'smooth' });
+    }
+  };
+
   return (
     <div className='h-40 space-y-0.5 md:space-y-2'>
       <h2 className='w-56 cursor-pointer text-sm font-semibold md:text-2xl text-[#d8d8d8c9] transition duration-200 hover:text-white'>
         {title}
       </h2>
       <div className='group relative md:-ml-2'>
-        <ChevronLeftIcon className='chevron' />
+        <ChevronLeftIcon
+          className={`chevron left-2 ${!isMoved && 'hidden'}`}
+          onClick={() => clickHandler('left')}
+        />
 
-        <div>{/* thumbnail */}</div>
+        <div
+          ref={rowRef}
+          className='flex scrollbar-hide items-center space-x-0.5 overflow-x-scroll md:space-x-2 md:p-2'
+        >
+          {movies.map((movie) => (
+            <Thumbnail key={movie.id} movie={movie} />
+          ))}
+        </div>
 
-        <ChevronRightIcon className='chevron' />
+        <ChevronRightIcon
+          className='chevron right-2'
+          onClick={() => clickHandler('right')}
+        />
       </div>
     </div>
   );
