29b814fa8995e2f5b799fb9ed6700b5b63952484
Birrellc <vaiezmedia@gmail.com>
2022-10-08 13:31:38 +0100

    Feat: created login form with error handling

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,36 +1,95 @@
-import React from 'react';
+import { useState } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
 import netflixLoginBg from '../assets/netflixLoginBg.jpg';
 import netflixLogo from '../assets/netflixLogo.png';
+import { useForm, SubmitHandler } from 'react-hook-form';
 
-interface Props {}
+interface Inputs {
+  email: string;
+  password: string;
+}
 
-const login = (props: Props) => {
+const login = () => {
+  const [login, setLogin] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+
   return (
     <div className='relative flex h-screen w-screen flex-col bg-black md:items-center md:justify-center md:bg-transparent'>
       <Head>
         <title>Netflix Login</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <div>
-        <Image
-          src={netflixLoginBg}
-          layout='fill'
-          className='-z-10 !hidden opacity-50 sm:!inline'
-          objectFit='cover'
-        />
-      </div>
-      <div>
-        <Image
-          src={netflixLogo}
-          alt='netflix logo'
-          height='150'
-          width='150'
-          objectFit='contain'
-          className='absolute left-6 top-6 cursor-pointer md:left-12 md:top-8'
-        />
-      </div>
+
+      <Image
+        src={netflixLoginBg}
+        layout='fill'
+        className='-z-10 !hidden opacity-50 sm:!inline'
+        objectFit='cover'
+      />
+
+      <Image
+        src={netflixLogo}
+        alt='netflix logo'
+        height={135}
+        width={135}
+        layout='fixed'
+        objectFit='contain'
+        className='cursor-pointer !top-4'
+      />
+
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className='relative mt-24 space-y-8 rounded bg-black/40 py-10 px-6 md:mt-5 md:max-w-md md:px-14'
+      >
+        <h1 className='text-4xl font-semibold'>Sign In</h1>
+        <div className='space-y-3'>
+          <label className='inline-block w-full'>
+            <input
+              type='email'
+              placeholder='Your Email'
+              className='input'
+              {...register('email', { required: true })}
+            />
+            {errors.email && (
+              <span className='text-red-500 italic font-semibold'>
+                Invalid Email
+              </span>
+            )}
+          </label>
+          <label className='inline-block w-full'>
+            <input
+              type='password'
+              placeholder='Password'
+              className='input'
+              {...register('password', { required: true })}
+            />
+            {errors.password && (
+              <span className='text-red-500 italic font-semibold'>
+                Invalid Password
+              </span>
+            )}
+          </label>
+        </div>
+
+        <button
+          type='submit'
+          className='w-full rounded bg-[#e50914] py-3 font-semibold'
+        >
+          Login
+        </button>
+
+        <button type='submit' className='w-full text-white hover:underline'>
+          Sign Up
+        </button>
+      </form>
     </div>
   );
 };
