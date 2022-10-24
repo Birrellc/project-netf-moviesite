c8b1dbe67ddc52cd1474903bd3e72822b4b911f3
Birrellc <vaiezmedia@gmail.com>
2022-10-24 14:11:23 +0100

    Feat: submit user sub purchase to firebase & stripe

--- components/SubPlans.tsx
+++ components/SubPlans.tsx
@@ -1,10 +1,14 @@
 import { CheckIcon } from '@heroicons/react/solid';
 import { Product } from '@stripe/firestore-stripe-payments';
+import { useState } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
 import Link from 'next/link';
 import netflixLogo from '../assets/netflixLogo.png';
 import useAuth from '../customHooks/useAuth';
+import Table from './Table';
+import Loader from './Loader';
+import { loadCheckout } from '../lib/stripe';
 
 // Product[] - type in stripe/firestore package
 interface Props {
@@ -13,7 +17,18 @@ interface Props {
 
 const SubPlans = ({ products }: Props) => {
   //
-  const { logout } = useAuth();
+  const { logout, user } = useAuth();
+  // on default premium will be highlighted
+  const [activePlan, setActivePlan] = useState<Product | null>(products[2]);
+  const [stripeLoading, setStripeLoading] = useState(false);
+
+  const subToPlan = () => {
+    // return if no user
+    if (!user) return;
+
+    loadCheckout(activePlan?.prices[0].id!);
+    setStripeLoading(true);
+  };
 
   return (
     <div>
@@ -40,6 +55,7 @@ const SubPlans = ({ products }: Props) => {
           Sign Out
         </button>
       </header>
+
       <main className='mx-auto max-w-5xl px-5 pt-32 pb-12 transition-all md:px-10'>
         <h1 className='mb-4 text-3xl font-semibold'>
           Choose your subscription plan
@@ -54,22 +70,43 @@ const SubPlans = ({ products }: Props) => {
           </li>
           <li className='flex items-center gap-x-2 text-lg'>
             <CheckIcon className='h-7 w-7 text-[#E50914]' /> Change or cancel
-            your plan anytime.
+            your plan at anytime.
           </li>
         </ul>
 
-        <div className='mt-4 flex flex-col space-y-4'>
-          <div className='flex w-full items-center justify-end self-end'>
+        <div className='mt-4 pt-6 flex flex-col space-y-4'>
+          <div className='flex w-full items-center justify-end self-end md:w-3/5"'>
             {products.map((product) => (
-              <div className='planContainer' key={product.id}>
+              <div
+                className={`planContainer ${
+                  activePlan?.id === product.id ? 'opacity-100' : 'opacity-50'
+                }`}
+                key={product.id}
+                onClick={() => setActivePlan(product)}
+              >
                 {product.name}
               </div>
             ))}
           </div>
 
-          {/* Table */}
+          <Table products={products} activePlan={activePlan} />
 
-          <button>Subscribe</button>
+          <button
+            disabled={!activePlan || stripeLoading}
+            className={`mx-auto w-11/12 rounded bg-[#E50914] py-4 text-xl shadow hover:bg-[#f6121d] md:w-[420px] ${
+              stripeLoading && 'opacity-60'
+            }`}
+            onClick={subToPlan}
+          >
+            {stripeLoading ? (
+              <Loader color='dark:fill-gray-300' />
+            ) : (
+              'Subscribe'
+            )}
+          </button>
+          <p className='flex justify-center italic text-xs'>
+            (can take a few moments)
+          </p>
         </div>
       </main>
     </div>
